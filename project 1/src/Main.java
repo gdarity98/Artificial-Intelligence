1f3631f4ef21214cbd90fa93060a0aeefe8cfbb4
Gabe Darity <gdarity98@gmail.com>
2021-09-13 13:57:47 -0600

    Update Main.java
    changed the declarations to not be C style lol idk

--- project 1/src/Main.java
+++ project 1/src/Main.java
@@ -2,11 +2,11 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String args[]) throws FileNotFoundException {
-        PuzzleImporter easyPuzzles[] = new PuzzleImporter[5];
-        Object medPuzzles[] = new Object[5];
-        Object hardPuzzles[] = new Object[5];
-        Object evilPuzzles[] = new Object[5];
+    public static void main(String[] args) throws FileNotFoundException {
+        PuzzleImporter[] easyPuzzles = new PuzzleImporter[5];
+        PuzzleImporter[] medPuzzles = new PuzzleImporter[5];
+        PuzzleImporter[] hardPuzzles = new PuzzleImporter[5];
+        PuzzleImporter[] evilPuzzles = new PuzzleImporter[5];
 
         for(int i = 0; i < easyPuzzles.length; i++){
             easyPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Easy-P"+(i+1)+".csv");
