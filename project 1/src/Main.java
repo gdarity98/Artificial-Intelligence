22feca9281400c33eeef76387061c892293e5af5
Gabe Darity <gdarity98@gmail.com>
2021-09-13 13:00:35 -0600

    Puzzle Importer Implementation
    Implemented the puzzle importer. Takes in the path to a .csv file that describes the spaces of a sudoku puzzle, puts all of that information into a matrix. Then implemented a getSudokuPuzzle and a printSudokuPuzzle so that we can access and view the Sudoku puzzles.

--- project 1/src/Main.java
+++ project 1/src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String args[]){
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String args[]) throws FileNotFoundException {
+        PuzzleImporter easy1 = new PuzzleImporter("project 1/sudoku puzzles/Easy-P1.csv");
+        easy1.printSudokuPuzzle();
     }
 }
--- project 1/src/PuzzleImporter.java
+++ project 1/src/PuzzleImporter.java
@@ -1,3 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PuzzleImporter {
-    
+        int[][] sudokuPuzzle = new int[9][9];
+        public PuzzleImporter(String file) throws FileNotFoundException {
+            Scanner scanner = new Scanner(new File(file));
+            scanner.useDelimiter("\n");
+            int i = 0;
+            int j = 0;
+            while(scanner.hasNext()){
+                String line = scanner.next();
+                line = line.replace('\r',' ');
+                line = line.trim();
+                String[] spaces = line.split(",");
+                for(int k = 0; k < spaces.length; k++){
+                    if (!spaces[k].contains("?")) {
+                    sudokuPuzzle[i][j] = Integer.parseInt(spaces[k]);
+                    }
+                    j++;
+                    if(j == 9){
+                        i++;
+                        j = 0;
+                    }
+                }
+            }
+            scanner.close();
+        }
+
+        public int[][] getSudokuPuzzle(){
+            return sudokuPuzzle;
+        }
+
+        public void printSudokuPuzzle(){
+            for(int[] x : sudokuPuzzle){
+                for(int y : x){
+                    System.out.print(y + "  ");
+                }
+                System.out.print("\n");
+            }
+        }
 }
