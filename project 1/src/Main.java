693e11b44a495a28dc39a195f9f3c9b8e57225ec
Gabe Darity <gdarity98@gmail.com>
2021-09-13 13:55:42 -0600

    Importing All Puzzles
    imports all the puzzles and groups them by difficulty into arrays.

--- project 1/src/Main.java
+++ project 1/src/Main.java
@@ -10,9 +10,22 @@ public class Main {
 
         for(int i = 0; i < easyPuzzles.length; i++){
             easyPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Easy-P"+(i+1)+".csv");
-            System.out.println("Sudoku Puzzle Easy " + (i+1));
-            easyPuzzles[i].printSudokuPuzzle();
-            System.out.println("");
+            //System.out.println("Sudoku Puzzle Easy " + (i+1));
+            //easyPuzzles[i].printSudokuPuzzle();
+            //System.out.println("");
+            medPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Med-P"+(i+1)+".csv");
+            //System.out.println("Sudoku Puzzle Medium " + (i+1));
+            //medPuzzles[i].printSudokuPuzzle();
+            //System.out.println("");
+            hardPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Hard-P"+(i+1)+".csv");
+            //System.out.println("Sudoku Puzzle Hard " + (i+1));
+            //hardPuzzles[i].printSudokuPuzzle();
+            //System.out.println("");
+            evilPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Evil-P"+(i+1)+".csv");
+            //System.out.println("Sudoku Puzzle Evil " + (i+1));
+            //evilPuzzles[i].printSudokuPuzzle();
+            //System.out.println("");
         }
+
     }
 }
