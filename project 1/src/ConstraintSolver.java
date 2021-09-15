ac997a64e12a14273a666bebd685582caf892e17
Gabe Darity <gdarity98@gmail.com>
2021-09-15 09:03:31 -0600

    Update ConstraintSolver.java
    Need functions to find conflicts

--- project 1/src/ConstraintSolver.java
+++ project 1/src/ConstraintSolver.java
@@ -4,4 +4,17 @@ public abstract class ConstraintSolver {
     public ConstraintSolver(PuzzleImporter array[]){
         puzzles = array;
     }
+
+    public void validateColumn(int puzzleIndex, int x, int y){
+
+    }
+
+    public void validateRow(int puzzleIndex, int x, int y){
+
+    }
+
+    public void validate3x3Box(int puzzleIndex, int x, int y){
+
+    }
+
 }
