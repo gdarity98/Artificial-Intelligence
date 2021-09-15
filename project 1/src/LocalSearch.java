c5929af6033d77ba5e7c49d3542982806a5e0743
Gabe Darity <gdarity98@gmail.com>
2021-09-15 10:16:39 -0600

    Temperature Scheduling SA
    Added the schedule algorithm to Local Search Simulated Annealing

--- project 1/src/LocalSearch.java
+++ project 1/src/LocalSearch.java
@@ -18,7 +18,9 @@ public class LocalSearch extends ConstraintSolver{
         // current, next, T local variables
         int[][] current = new int[1][2];
         int[][] next = new int[1][2];
-        int T = 1000000; //I have no idea how this temp thing works rn
+        int T0 = 1000000; //I have no idea how this temp thing works rn
+        int Tt;
+
 
         //initialize the current node with the puzzle we have (random state?)
         // I am initializing all puzzles in the puzzle array
@@ -28,13 +30,14 @@ public class LocalSearch extends ConstraintSolver{
 
         //for i until inf or until a set fitness or number of runs or something that stops the program
         for(int i = 0; i < 1000000000; i ++) {
+            
             //Set the Temperature for the iteration we are on
             //schedule slowly reduces the temp down to 0
             int t = i;
-            T = schedule(t);
+            Tt = schedule(t, T0);
 
             //if temp = 0
-            if(T == 0){
+            if(Tt == 0){
                 //   return current (idk if current is a space or a puzzle state lmfao)
                 //   idk whats happening big fun
                 return puzzles;
@@ -76,7 +79,10 @@ public class LocalSearch extends ConstraintSolver{
         puzzle.setSudokuPuzzle(newPuzzle);
     }
 
-    public int schedule(int t){
-
+    public int schedule(int t, int T0){
+        int nt = t;
+        int tunedParameter = 1;
+        int Tt = ((T0*tunedParameter)/(tunedParameter + nt));
+        return Tt;
     }
 }
