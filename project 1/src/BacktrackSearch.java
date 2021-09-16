0261d1694ebf55d9063cfe0777066786e74f0fcb
Kyler <kyler.gappa@student.montana.edu>
2021-09-15 22:51:53 -0600

    SimpleBacktrack works need to comment still

--- project 1/src/BacktrackSearch.java
+++ project 1/src/BacktrackSearch.java
@@ -2,39 +2,98 @@ import java.util.Scanner;
 
 public class BacktrackSearch {
 
+    int[][] puzzleToSolve;
 
-    public static int[][] SimpleBacktrackSearch(int[][] puzzleToSolve, int n) {
-        int row = -1;
-        int column = -1;
-        //Return when the end of the puzzle is reached
-        if (row == 8 && column == 9) {
-            return puzzleToSolve;
-        }
-
-        //At the end of each row, move down and left
-        if (column == 9) {
-            row++;
-            column = 0;
-        }
-
-        //Skip the given values
-        if (puzzleToSolve[row][column] != 0) {
-            return SimpleBacktrackSearch(puzzleToSolve, n);
-        }
-
-        //loop this to assign 1-9 or loop over whole puzzle N-queens style???
-        if (LegalValue(puzzleToSolve, row, column, n)) {
-            puzzleToSolve[row][column] = 1;
-        }
-
-
-        //here while writing so no error is thrown
-        return new int[9][9];
-
+    public BacktrackSearch(int[][] puzzleToSolve) {
+        this.puzzleToSolve = puzzleToSolve;
     }
 
-    private static boolean LegalValue(int[][] puzzleToSolve, int row, int column, int n) {
-        //Constraints go here
+    public boolean SimpleBacktrackSearch(int[][] puzzleToSolve, int n) {
+        int row = -1;
+        int column = -1;
+        boolean empty = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (puzzleToSolve[i][j] == 0) {
+                    row = i;
+                    column = j;
+                    empty = false;
+                    break;
+                }
+            }
+            if (!empty) {
+                break;
+            }
+        }
+
+        if (empty) {
+            return true;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            if (LegalValue(puzzleToSolve, row, column, i)) {
+                puzzleToSolve[row][column] = i;
+                if (SimpleBacktrackSearch(puzzleToSolve, n)) {
+                    return true;
+                } else {
+                    puzzleToSolve[row][column] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean LegalValue(int[][] puzzleToSolve, int row, int column, int possibleValue) {
+        int boxRow = 0;
+        int boxColumn = 0;
+
+        //check each row for a duplicate value
+        for (int i = 0; i < puzzleToSolve.length; i++) {
+            if (puzzleToSolve[row][i] == possibleValue) {
+                return false;
+            }
+        }
+
+        //check each column for a duplicate value
+        for (int i = 0; i < puzzleToSolve.length; i++) {
+            if (puzzleToSolve[i][column] == possibleValue) {
+                return false;
+            }
+        }
+
+        //get the correct first row of the square
+        if (row < 3) {
+            boxRow = 0;
+        }
+        if (row >= 3 && row < 6) {
+            boxRow = 3;
+        }
+        if (row >= 6) {
+            boxRow = 6;
+        }
+
+        //get the correct first column of the square
+        if (column < 3) {
+            boxColumn = 0;
+        }
+        if (column >= 3 && column < 6) {
+            boxColumn = 3;
+        }
+        if (column >= 6) {
+            boxColumn = 6;
+        }
+
+        //check the box/group for duplicates
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxColumn; j < boxColumn + 3; j++) {
+                if (puzzleToSolve[i][j] == possibleValue) {
+                    return false;
+                }
+            }
+        }
+
+        //congrats, there are no restrictions
         return true;
     }
 
--- project 1/src/Main.java
+++ project 1/src/Main.java
@@ -2,29 +2,37 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws FileNotFoundException {
         PuzzleImporter[] easyPuzzles = new PuzzleImporter[5];
         PuzzleImporter[] medPuzzles = new PuzzleImporter[5];
         PuzzleImporter[] hardPuzzles = new PuzzleImporter[5];
         PuzzleImporter[] evilPuzzles = new PuzzleImporter[5];
 
-        for(int i = 0; i < easyPuzzles.length; i++){
-            easyPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Easy-P"+(i+1)+".csv");
-            //System.out.println("Sudoku Puzzle Easy " + (i+1));
+        for (int i = 0; i < easyPuzzles.length; i++) {
+            easyPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Easy-P" + (i + 1) + ".csv");
+            //System.out.println("Sudoku Puzzle Easy " + (i + 1));
             //easyPuzzles[i].printSudokuPuzzle();
             //System.out.println("");
-            medPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Med-P"+(i+1)+".csv");
+            medPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Med-P" + (i + 1) + ".csv");
             //System.out.println("Sudoku Puzzle Medium " + (i+1));
             //medPuzzles[i].printSudokuPuzzle();
             //System.out.println("");
-            hardPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Hard-P"+(i+1)+".csv");
+            hardPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Hard-P" + (i + 1) + ".csv");
             //System.out.println("Sudoku Puzzle Hard " + (i+1));
             //hardPuzzles[i].printSudokuPuzzle();
             //System.out.println("");
-            evilPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Evil-P"+(i+1)+".csv");
+            evilPuzzles[i] = new PuzzleImporter("project 1/sudoku puzzles/Evil-P" + (i + 1) + ".csv");
             //System.out.println("Sudoku Puzzle Evil " + (i+1));
             //evilPuzzles[i].printSudokuPuzzle();
             //System.out.println("");
+        }
+        for (int i = 0; i < easyPuzzles.length; i++) {
+            BacktrackSearch backtrackSearch = new BacktrackSearch(evilPuzzles[i].getSudokuPuzzle());
+            System.out.println(backtrackSearch.SimpleBacktrackSearch(evilPuzzles[i].getSudokuPuzzle(), evilPuzzles[i].getSudokuPuzzle().length));
+            System.out.println("Sudoku Puzzle Easy " + (i + 1));
+            evilPuzzles[i].printSudokuPuzzle();
+            System.out.println("");
         }
 
         // LocalSearch ls = new LocalSearch(easyPuzzles, "simulatedAnnealing");
