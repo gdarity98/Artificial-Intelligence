dc173d772292c38e1e2ea719567891ce9952737e
gdarity98 <gdarity98@gmail.com>
2021-09-17 16:50:27 -0600

    Update PuzzleImporter.java
    bug on my computer involving UTF8_BOM at substring 1, not ï»¿ at substring 3
    
    added both

--- project 1/src/PuzzleImporter.java
+++ project 1/src/PuzzleImporter.java
@@ -26,6 +26,9 @@ public class PuzzleImporter {
                     // https://stackoverflow.com/questions/21891578/removing-bom-characters-using-java
                     // https://stackoverflow.com/questions/40311348/numberformatexception-with-integer-parseint
                     String UTF8_BOM = "\uFEFF";
+                    if (spaces[k].startsWith(UTF8_BOM)) {
+                        spaces[k] = spaces[k].substring(1);
+                    }
                     if (spaces[k].startsWith("ï»¿")) {
                         spaces[k] = spaces[k].substring(3);
                     }
