1a669907bc7a797c19ffc2f60468591205b9796d
brocklehnen <36985454+brocklehnen@users.noreply.github.com>
2021-10-13 18:17:15 -0600

    Update ResolutionBased.java

--- project 2/src/ResolutionBased.java
+++ project 2/src/ResolutionBased.java
@@ -50,37 +50,63 @@ public class ResolutionBased {
             System.out.println(senses);
            
              
-            if (senses[3]){
-            safe[playerPosition[0]] [playerPosition[1]] = 1;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
-            }
-            if (senses[2]) {
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[2]) { //gold
+                System.out.println(2);
+           
             setGoldFound(1);
             }
-            if(senses[1]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if(senses[1]){ //pit
+                System.out.println(3);
+            
             }
-            if (senses[0] && senses[1]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[0] && senses[1]){ //pit and wumpus
+                System.out.println(4);
+          
             }
-            if (senses[0]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[0]){ //wumpus
+                System.out.println(5);
+           
             }
-            if (senses[0] && senses[2]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[0] && senses[2]){ //wumpus and gold
+            System.out.println(6);
+               
             }
-            if (senses[1] && senses[2]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[1] && senses[2]){ //pit and gold
+                System.out.println(7);
+           
             }
-            if (senses[0] && senses[1] && senses[2]){
-            safe[playerPosition[0]] [playerPosition[1]] = 0;
-            frontier[playerPosition[0]] [playerPosition[1]] = 0;
+            if (senses[0] && senses[1] && senses[2]){ //all three
+            System.out.println(8);
+            
+            }
+            if (!senses[2]) { //not gold
+                System.out.println(21);
+            
+           
+            }
+            if(!senses[1]){ //not pit
+                System.out.println(31);
+        
+            }
+            if (!senses[0] && !senses[1]){ //not pit or wumpus
+                System.out.println(41);
+            
+            }
+            if (!senses[0]){ //not wumpus
+                System.out.println(51);
+          
+            }
+            if (!senses[0] && !senses[2]){ //not gold or wumpus
+            System.out.println(61);
+               
+            }
+            if (!senses[1] && !senses[2]){ //not gold or pit
+                System.out.println(71);
+            
+            }
+            if (!senses[0] && !senses[1] && !senses[2]){ //none of them
+            System.out.println(81);
+                
             }
             //TODO get a rule based on the senses to send into reasoning? Brock
             //if smell, then there exists a wumpus in the surroundingSpaces
